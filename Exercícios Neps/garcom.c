e5eef1f18d8d4376127b78eb7678aeb553bac3d9
Renato Bressan <renatoikeda98@gmail.com>
2025-11-02 12:22:55 -0300

    Add files via upload

---
+++ Exercícios Neps/garcom.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define N 100
+
+int main() {
+    int n, l[N], c[N], quebrados = 0;
+    scanf("%d", &n);
+    if (n < 1 || n > N) return 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &l[i], &c[i]);
+        if (l[i] < 0 || l[i] > N || c[i] < 0 || c[i] > N) return 0;
+        if (l[i] > c[i]) quebrados += c[i];
+    }
+    printf("%d\n", quebrados);
+    return 0;
+}
