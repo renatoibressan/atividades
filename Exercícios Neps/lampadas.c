1d0153e0cc0cf39c0f4a5dc5d2c4e9981fe45a52
Renato Bressan <renatoikeda98@gmail.com>
2025-11-01 23:20:49 -0300

    Create lampadas.c

---
+++ Exercícios Neps/lampadas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, v[100000], i1 = 0, i2 = 0, la = 0, lb = 0;
+    scanf("%d", &n);
+    if (n < 2 || n > 100000) return 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+        if (v[i] == 1) i1++;
+        else if (v[i] == 2) i2++;
+        else return 0;
+    }
+    la = (i1 + i2) % 2;
+    lb = i2 % 2;
+    printf("%d\n%d\n", la, lb);
+    return 0;
+}
