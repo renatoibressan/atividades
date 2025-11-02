d5615932321072c037f9fa555ee4fcedb6614cce
Renato Bressan <renatoikeda98@gmail.com>
2025-11-02 12:02:14 -0300

    Add files via upload

---
+++ Exercícios Neps/premio_do_milhao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, a[1000], soma = 0, count = 0;
+    scanf("%d", &n);
+    if (n < 1 || n > 1000) return 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        if (a[i] < 0 || a[i] > 1000000) return 0;
+    }
+    for (int i = 0; i < n; i++) {
+        soma += a[i];
+        if (soma >= 1000000) {
+            count = i + 1;
+            break;
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
